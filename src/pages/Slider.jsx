ccb145332498851ee01f83402c2e23fb61d2f976
Sudiptacoding <you@example.comsudiptabiswas506@gmail.com>
2023-11-03 12:44:41 +0600

    Slider added

---
+++ src/pages/Slider.jsx
@@ -0,0 +1,11 @@
+
+//  I use two npm pakage for slider
+
+// First visit 
+
+https://www.npmjs.com/package/react-slideshow-image
+
+
+// Second slider webside link
+
+https://react-slideshow-image.netlify.app/?path=/story/introduction--page
